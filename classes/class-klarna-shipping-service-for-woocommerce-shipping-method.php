a160ae15991c3674548bc89abe8755ae46346bfd
MichaelBengtsson <michael@krokedil.se>
2020-02-14 08:29:53 +0100

    Wrapped our shipping method class in class exists

--- classes/class-klarna-shipping-service-for-woocommerce-shipping-method.php
+++ classes/class-klarna-shipping-service-for-woocommerce-shipping-method.php
@@ -9,104 +9,107 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-/**
- * Shipping method class.
- */
-class Klarna_Shipping_Serivce_For_WooCommerce_Shipping_Method extends WC_Shipping_Method {
+if ( class_exists( 'WC_Shipping_Method' ) ) {
 
 	/**
-	 * Class constructor.
-	 *
-	 * @param integer $instance_id The instance id.
+	 * Shipping method class.
 	 */
-	public function __construct( $instance_id = 0 ) {
-		$this->id                 = 'klarna_kss';
-		$this->instance_id        = absint( $instance_id );
-		$this->title              = 'Klarna Shipping Service';
-		$this->method_title       = __( 'Klarna Shipping Service', 'klarna-shipping-service-for-woocommerce' );
-		$this->method_description = __( 'Enables Klarna Shipping Service for WooCommerce', 'klarna-shipping-service-for-woocommerce' );
-		$this->supports           = array(
-			'shipping-zones',
-		);
-		$this->kss_tax_amount     = false;
-		add_filter( 'woocommerce_shipping_packages', array( $this, 'kss_add_tax' ) );
-	}
+	class Klarna_Shipping_Serivce_For_WooCommerce_Shipping_Method extends WC_Shipping_Method {
 
-	/**
-	 * Check if shipping method should be available.
-	 *
-	 * @param array $package The shipping package.
-	 * @return boolean
-	 */
-	public function is_available( $package ) {
-		if ( null !== WC()->session->get( 'kco_kss_enabled' ) && WC()->session->get( 'kco_kss_enabled' ) ) {
-			return true;
+		/**
+		 * Class constructor.
+		 *
+		 * @param integer $instance_id The instance id.
+		 */
+		public function __construct( $instance_id = 0 ) {
+			$this->id                 = 'klarna_kss';
+			$this->instance_id        = absint( $instance_id );
+			$this->title              = 'Klarna Shipping Service';
+			$this->method_title       = __( 'Klarna Shipping Service', 'klarna-shipping-service-for-woocommerce' );
+			$this->method_description = __( 'Enables Klarna Shipping Service for WooCommerce', 'klarna-shipping-service-for-woocommerce' );
+			$this->supports           = array(
+				'shipping-zones',
+			);
+			$this->kss_tax_amount     = false;
+			add_filter( 'woocommerce_shipping_packages', array( $this, 'kss_add_tax' ) );
 		}
-		return false;
-	}
 
-	/**
-	 * Calculate shipping cost.
-	 *
-	 * @param array $package The shipping package.
-	 * @return void
-	 */
-	public function calculate_shipping( $package = array() ) {
-		$label = 'Klarna Shipping Service';
-		$cost  = 0;
-		if ( null !== WC()->session->get( 'kco_wc_order_id' ) ) {
-			$klarna_order = KCO_WC()->api->get_order();
-			if ( isset( $klarna_order->selected_shipping_option ) ) {
-				$label                = $klarna_order->selected_shipping_option->name;
-				$cost                 = floatval( $klarna_order->selected_shipping_option->price - $klarna_order->selected_shipping_option->tax_amount ) / 100;
-				$tax_amount           = floatval( $klarna_order->selected_shipping_option->tax_amount ) / 100;
-				$this->kss_tax_amount = $tax_amount;
-
-				$rate = array(
-					'id'    => $this->get_rate_id(),
-					'label' => $label,
-					'cost'  => $cost,
-				);
+		/**
+		 * Check if shipping method should be available.
+		 *
+		 * @param array $package The shipping package.
+		 * @return boolean
+		 */
+		public function is_available( $package ) {
+			if ( null !== WC()->session->get( 'kco_kss_enabled' ) && WC()->session->get( 'kco_kss_enabled' ) ) {
+				return true;
 			}
-			$this->add_rate( $rate );
+			return false;
 		}
-	}
 
-	/**
-	 * Add tax amount to shipping.
-	 *
-	 * @param array $packages packages.
-	 * @return array
-	 */
-	public function kss_add_tax( $packages ) {
-		if ( false !== $this->kss_tax_amount ) {
-			foreach ( $packages as $i => $package ) {
-				foreach ( $package['rates'] as $rate_key => $rate_values ) {
-					if ( 'klarna_kss' === $rate_values->method_id ) { // check that the shipping is KSS.
-						$taxes = array();
-						foreach ( $package['rates'][ $rate_key ]->taxes as $key => $tax ) {
-							// set the KSS tax amount in the taxes array.
-							$taxes[ $key ] = $this->kss_tax_amount;
+		/**
+		 * Calculate shipping cost.
+		 *
+		 * @param array $package The shipping package.
+		 * @return void
+		 */
+		public function calculate_shipping( $package = array() ) {
+			$label = 'Klarna Shipping Service';
+			$cost  = 0;
+			if ( null !== WC()->session->get( 'kco_wc_order_id' ) ) {
+				$klarna_order = KCO_WC()->api->get_order();
+				if ( isset( $klarna_order->selected_shipping_option ) ) {
+					$label                = $klarna_order->selected_shipping_option->name;
+					$cost                 = floatval( $klarna_order->selected_shipping_option->price - $klarna_order->selected_shipping_option->tax_amount ) / 100;
+					$tax_amount           = floatval( $klarna_order->selected_shipping_option->tax_amount ) / 100;
+					$this->kss_tax_amount = $tax_amount;
+
+					$rate = array(
+						'id'    => $this->get_rate_id(),
+						'label' => $label,
+						'cost'  => $cost,
+					);
+				}
+				$this->add_rate( $rate );
+			}
+		}
+
+		/**
+		 * Add tax amount to shipping.
+		 *
+		 * @param array $packages packages.
+		 * @return array
+		 */
+		public function kss_add_tax( $packages ) {
+			if ( false !== $this->kss_tax_amount ) {
+				foreach ( $packages as $i => $package ) {
+					foreach ( $package['rates'] as $rate_key => $rate_values ) {
+						if ( 'klarna_kss' === $rate_values->method_id ) { // check that the shipping is KSS.
+							$taxes = array();
+							foreach ( $package['rates'][ $rate_key ]->taxes as $key => $tax ) {
+								// set the KSS tax amount in the taxes array.
+								$taxes[ $key ] = $this->kss_tax_amount;
+							}
+							// Set the tax amount.
+							$package['rates'][ $rate_key ]->taxes = $taxes;
 						}
-						// Set the tax amount.
-						$package['rates'][ $rate_key ]->taxes = $taxes;
 					}
 				}
 			}
+			return $packages;
 		}
-		return $packages;
+
 	}
 
-}
-
-add_filter( 'woocommerce_shipping_methods', 'add_kss_shipping_method' );
-/**
- * Registers the shipping method.
- *
- * @param array $methods WooCommerce shipping methods.
- * @return array
- */
-function add_kss_shipping_method( $methods ) {
-	$methods['klarna_kss'] = 'Klarna_Shipping_Serivce_For_WooCommerce_Shipping_Method';
-	return $methods;
+	add_filter( 'woocommerce_shipping_methods', 'add_kss_shipping_method' );
+	/**
+	 * Registers the shipping method.
+	 *
+	 * @param array $methods WooCommerce shipping methods.
+	 * @return array
+	 */
+	function add_kss_shipping_method( $methods ) {
+		$methods['klarna_kss'] = 'Klarna_Shipping_Serivce_For_WooCommerce_Shipping_Method';
+		return $methods;
+	}
 }
