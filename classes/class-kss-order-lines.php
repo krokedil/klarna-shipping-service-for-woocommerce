5bacbe5ac4fb8c19e8c1bc7287822274e3091569
Michael Bengtsson <michael@krokedil.se>
2020-12-04 13:24:09 +0100

    Add shipping class to products, change how we add dimensions.

--- classes/class-kss-order-lines.php
+++ classes/class-kss-order-lines.php
@@ -17,43 +17,22 @@ class KSS_Order_Lines {
 	 * Class constructor.
 	 */
 	public function __construct() {
-		add_filter( 'kco_wc_api_request_args', array( $this, 'add_product_dimensions' ) );
+		add_filter( 'kco_wc_cart_line_item', array( $this, 'add_product_dimensions' ), 10, 2 );
+		add_filter( 'kco_wc_cart_line_item', array( $this, 'add_shipping_class' ), 10, 2 );
 	}
 
 	/**
-	 * Adds the product dimensions to the object.
+	 * Maybe add the shipping class to the Klarna item.
 	 *
-	 * @param array $request_args The request args to Klarna.
+	 * @param array $klarna_item The Klarna Item.
+	 * @param array $cart_item The Cart Item.
 	 * @return array
 	 */
-	public function add_product_dimensions( $request_args ) {
-		$order_lines     = $request_args['order_lines'];
-		$new_order_lines = array();
-		foreach ( $order_lines as $order_line ) {
-			$shipping_attributes = $this->get_shipping_attributes_for_product( $order_line['reference'] );
-			if ( false !== $shipping_attributes ) {
-				$order_line['shipping_attributes'] = $shipping_attributes;
-			}
-			$new_order_lines[] = $order_line;
-		}
-		$request_args['order_lines'] = $new_order_lines;
-		return $request_args;
-	}
-
-	/**
-	 * Gets the shipping attributes for the product.
-	 *
-	 * @param string $sku The product SKU.
-	 * @return array
-	 */
-	public function get_shipping_attributes_for_product( $sku ) {
-		if ( 0 !== wc_get_product_id_by_sku( $sku ) ) {
-			$product = wc_get_product( wc_get_product_id_by_sku( $sku ) );
+	public function add_product_dimensions( $klarna_item, $cart_item ) {
+		if ( $cart_item['variation_id'] ) {
+			$product = wc_get_product( $cart_item['variation_id'] );
 		} else {
-			$product = wc_get_product( $sku );
-		}
-		if ( ! $product ) {
-			return false;
+			$product = wc_get_product( $cart_item['product_id'] );
 		}
 
 		$product_weight = ! empty( $product->get_weight() ) ? $product->get_weight() : 0;
@@ -61,22 +40,36 @@ class KSS_Order_Lines {
 		$product_width  = ! empty( $product->get_width() ) ? $product->get_width() : 0;
 		$product_length = ! empty( $product->get_length() ) ? $product->get_length() : 0;
 
-		$shipping_attributes = array(
-			'weight'     => round( wc_get_weight( $product_weight, 'g' ) ),
-			'dimensions' => array(
-				'height' => round( wc_get_dimension( $product_height, 'mm' ) ),
-				'width'  => round( wc_get_dimension( $product_width, 'mm' ) ),
-				'length' => round( wc_get_dimension( $product_length, 'mm' ) ),
-			),
+		$klarna_item['shipping_attributes']['weight']     = round( wc_get_weight( $product_weight, 'g' ) );
+		$klarna_item['shipping_attributes']['dimensions'] = array(
+			'height' => round( wc_get_dimension( $product_height, 'mm' ) ),
+			'width'  => round( wc_get_dimension( $product_width, 'mm' ) ),
+			'length' => round( wc_get_dimension( $product_length, 'mm' ) ),
 		);
 
-		// Return array without any empty values.
-		return array_filter(
-			$shipping_attributes,
-			function( $value ) {
-				return ! is_null( $value ) && '' !== $value;
-			}
-		);
+		return $klarna_item;
+	}
+
+	/**
+	 * Maybe add the shipping class to the Klarna item.
+	 *
+	 * @param array $klarna_item The Klarna Item.
+	 * @param array $cart_item The Cart Item.
+	 * @return array
+	 */
+	public function add_shipping_class( $klarna_item, $cart_item ) {
+		$tags = array();
+		if ( $cart_item['variation_id'] ) {
+			$product = wc_get_product( $cart_item['variation_id'] );
+		} else {
+			$product = wc_get_product( $cart_item['product_id'] );
+		}
+		$shipping_class = $product->get_shipping_class();
+		if ( ! empty( $shipping_class ) ) {
+			$tags[]                                     = $shipping_class;
+			$klarna_item['shipping_attributes']['tags'] = $tags;
+		}
+		return $klarna_item;
 	}
 
 } new KSS_Order_Lines();
