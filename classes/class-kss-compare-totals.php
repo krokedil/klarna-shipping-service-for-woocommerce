2b8a6b4ea3c39f7668a090ae7271a0894780deb3
Kontrazen <krlovic@gmail.com>
2022-08-05 16:07:52 +0200

    Fix coding standard

--- classes/class-kss-compare-totals.php
+++ classes/class-kss-compare-totals.php
@@ -13,11 +13,12 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Free Orders class
  */
 class KSS_Compare_Totals {
+
 	/**
 	 * Class constructor.
 	 */
 	public function __construct() {
-		add_action( 'kco_wc_process_payment', array( $this, 'compare_kco_kss_totals' ), 10, 2 );
+		 add_action( 'kco_wc_process_payment', array( $this, 'compare_kco_kss_totals' ), 10, 2 );
 	}
 
 	/**
@@ -34,11 +35,12 @@ class KSS_Compare_Totals {
 		$klarna_total_amount = round( $klarna_order['selected_shipping_option']['price'] / 100, 2 );
 		$order_total_amount  = ( $order->get_shipping_total() + $order->get_shipping_tax() );
 
-		if ( $klarna_total_amount > $order_total_amount && ( $klarna_total_amount - $order_total_amount ) > 3 ) {
+		$dif = $klarna_total_amount - $order_total_amount;
+
+		if ( $dif > 3 || $dif < -3 ) {
 			$order->add_order_note( sprintf( __( 'A discrepancy between the Klarna order\'s shipping tax and the WooCommerce shipping tax has been detected. Please verify that you have set up your %1$s and %2$s according to the instructions given in the plugin documentation  ', 'klarna-checkout-for-woocommerce' ), ( '<a href="https://docs.krokedil.com/klarna-for-woocommerce/additional-klarna-plugins/klarna-shipping-assistant/#configuration" target="_blank">configuration</a>' ), ( '<a href="https://docs.krokedil.com/klarna-for-woocommerce/additional-klarna-plugins/klarna-shipping-assistant/#tax-settings" target="_blank">tax settings</a>' ) ) );
 		}
 	}
-
 }
 
 new KSS_Compare_Totals();
