8299722b491106054e2a98c3d0dd565fea8df365
anyakro <anya@krokedil.se>
2025-08-04 13:45:24 +0200

    Rebrand class comment

--- classes/class-kss-edit-klarna-order.php
+++ classes/class-kss-edit-klarna-order.php
@@ -1,6 +1,6 @@
 <?php // phpcs:ignore
 /**
- * Edit klarna order class.
+ * Edit kustom order class.
  *
  * @package KlarnaShippingService/Classes
  */
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Edit klarna order class.
+ * Edit kustom order class.
  */
 class KSS_Edit_Klarna_Order {
 	/**
